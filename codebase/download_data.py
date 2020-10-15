9d4cc3f4c17a7ed5aaaebf9735410342744a3433
Talvany Carlotto <talvany.carlotto@complyadvantage.com>
2020-10-15 19:21:01 +0100

    Fixed download data and settings

--- codebase/download_data.py
+++ codebase/download_data.py
@@ -1,75 +1,118 @@
 """
-This module downloads data for data training:
-- the files necessary to run XLNet using huggingface
-- the original csv for the training data: training_balanced.csv
-- the new datasets generated based on the original dataset
+This module downloads data for data training and for model execution
 """
 import click
 import wget
 import gdown
 import tarfile
 import os
+from codebase.log import logger
+from codebase.settings import DATA_PATH, OUT_MODELS_PATH
 
 
-def download_data(gdrive_ids, download_xlnet=True, multimodel=False, balanced=False):
-    if download_xlnet:
-        path_base = (
-            "https://s3.amazonaws.com/models.huggingface.co/bert/xlnet-base-cased"
+# the ids of the gdrive files for different modes
+GDRIVE_IDS = {
+    'main_dataset' : [
+        ("1kdFlCq9VWsQBhRBlijPis3a2Vhv220p5", DATA_PATH, "classification_dataset.csv"),
+        ("1TDiZ9mn9z9J7dTSx9WCz8BURx6yT7IPk", DATA_PATH, "training_all.csv"),
+        ("17DaIICz1c1qkZbrdR9qgipXJ-WCye0ow", DATA_PATH, "test.csv"),
+    ],
+    'train_multi' : [
+        ("1MvHYpXICuDSmq4jenK9j2_x54hrO21_k", DATA_PATH, "training_c1.csv"),
+        ("1E0lL9ZHwNtwZJsVo6iLirNJ7gtkOgFjr", DATA_PATH, "training_c2.csv"),
+        ("18hwo7ParTOvlV9WpPrb38Ox73DcCOnYV", DATA_PATH, "training_c3.csv"),
+        ("1ZrvkETgLAaYLyfUl47cWGmtIQpQlhDuD", DATA_PATH, "training_c4.csv"),
+        ("1uHvwuTxPDrEUzHHirHKS5U5wCLVOUpQ2", DATA_PATH, "training_c5.csv"),
+        ("1BBAUiCQnwjK42Fz2Kaih-DmCoVJX6Dtn", DATA_PATH, "training_c6.csv"),
+        ("1dKL9O6W75s3qsVQw56C8T8XeeAKIY4ir", DATA_PATH, "training_c7.csv")
+    ],
+    'train_single' : [
+        ("1vAhqiJXBTHp2sb4fOegJrKhZzM3RIGYc", DATA_PATH, "training_balanced.csv"),
+    ],
+    'predict_single': [
+        ("1IDQ-cuqCIqPrZ15V49L9TnggvBSZXc3_", OUT_MODELS_PATH, "balanced.tar.gz")
+    ],
+    'predict_multi': [
+        ("1-KG97zumrdzkkEKMT24vy5MeN971bN1f", OUT_MODELS_PATH, "multiclass.tar.gz")
+    ]
+}
+
+def download_gdrive_docs(gdrive_ids):
+    """
+    Downloads a list of documents from google drive
+    :param gdrive_ids: list of tuples (<gdrive id>, <destination dir>, <output file name>)
+    :return:
+    """
+    for doc_id, directory, output_name in gdrive_ids:
+        # only downloads if file does not exist
+        output_filepath = os.path.join(directory, output_name)
+        if not os.path.isfile(output_filepath):
+            logger.info(f"Downloading {output_filepath}")
+            url = f"https://drive.google.com/uc?id={doc_id}"
+            gdown.download(url, output_filepath, quiet=False)
+
+
+
+def download_and_extract(gdrive_ids):
+    """
+    Downloads from gdrive and extracts
+    :param gdrive_ids: list of tuples (<gdrive id>, <destination dir>, <output file name>)
+    :return:
+    """
+    for doc_id, directory, output_name in gdrive_ids:
+        file_path = os.path.join(directory, output_name)
+
+        download_gdrive_docs([(doc_id, file_path)])
+
+        logger.info(f"Extracting {file_path}")
+        with tarfile.open(file_path) as my_tar:
+            my_tar.extractall(directory)
+
+
+def download_xlnet_data():
+    """
+    Download data for xlnet training
+    :return:
+    """
+    path_base = (
+        "https://s3.amazonaws.com/models.huggingface.co/bert/xlnet-base-cased"
+    )
+
+    filenames = ["spiece.model", "pytorch_model.bin", "config.json"]
+
+    for name in filenames:
+        logger.info(f'Downloading {name} for xlnet')
+        wget.download(
+            f"{path_base}-{name}", out=f"../models/xlnet-base-cased/{name}"
         )
 
-        filenames = ["spiece.model", "pytorch_model.bin", "config.json"]
-
-        for name in filenames:
-            print(name)
-            wget.download(
-                f"{path_base}-{name}", out=f"../models/xlnet-base-cased/{name}"
-            )
-    if multimodel:
-        doc_id = '1-hvWGEeRECSK8WQPwfzjlSePf3MUjFnI'
-        directory = "../models/out/"
-        file_name = "multiclass.tar.gz"
-        file_path = os.path.join(directory, file_name)
-
-        url = f"https://drive.google.com/uc?id={doc_id}"
-        gdown.download(url, file_path, quiet=False)
-
-        with tarfile.open(file_path) as my_tar:
-            my_tar.extractall(directory)
-
-    if balanced:
-        doc_id = "1IDQ-cuqCIqPrZ15V49L9TnggvBSZXc3_"
-        directory = "../models/out/"
-        file_name = "balanced.tar.gz"
-        file_path = os.path.join(directory, file_name)
-
-        url = f"https://drive.google.com/uc?id={doc_id}"
-        gdown.download(url, file_path, quiet=False)
-
-        with tarfile.open(file_path) as my_tar:
-            my_tar.extractall(directory)
-
-    for doc_id, output in gdrive_ids:
-        url = f"https://drive.google.com/uc?id={doc_id}"
-        gdown.download(url, output, quiet=False)
-
-
 @click.command()
-def main():
-    gdrive_ids = [
-        # ("1kdFlCq9VWsQBhRBlijPis3a2Vhv220p5", "../data/classification_dataset.csv"),
-        # ("1TDiZ9mn9z9J7dTSx9WCz8BURx6yT7IPk", "../data/training_all.csv"),
-        # ("17DaIICz1c1qkZbrdR9qgipXJ-WCye0ow", "../data/test.csv"),
-        # ("1MvHYpXICuDSmq4jenK9j2_x54hrO21_k", "../data/training_c1.csv"),
-        # ("1E0lL9ZHwNtwZJsVo6iLirNJ7gtkOgFjr", "../data/training_c2.csv"),
-        # ("18hwo7ParTOvlV9WpPrb38Ox73DcCOnYV", "../data/training_c3.csv"),
-        # ("1ZrvkETgLAaYLyfUl47cWGmtIQpQlhDuD", "../data/training_c4.csv"),
-        # ("1uHvwuTxPDrEUzHHirHKS5U5wCLVOUpQ2", "../data/training_c5.csv"),
-        # ("1BBAUiCQnwjK42Fz2Kaih-DmCoVJX6Dtn", "../data/training_c6.csv"),
-        # ("1dKL9O6W75s3qsVQw56C8T8XeeAKIY4ir", "../data/training_c7.csv"),
-        # ("1vAhqiJXBTHp2sb4fOegJrKhZzM3RIGYc", "../data/training_balanced.csv"),
-    ]
-    download_data(gdrive_ids, download_xlnet=False, multimodel=True, balanced=True)
-    print("Finished downloading data")
+@click.option(
+    "--mode",
+    required=True,
+    help="The mode indicating what to download.",
+    type=click.Choice(
+        ["prediction_models_single", "prediction_models_multi", "train_data_single", "train_data_multi"]
+    ),
+)
+def main(mode):
+    logger.info(f'Downloading data for mode {mode}')
+
+    # download the main datasets in all modes
+    download_gdrive_docs(GDRIVE_IDS['main_dataset'])
+
+    if mode == "prediction_models_single":
+        download_and_extract(GDRIVE_IDS['predict_single'])
+    elif mode == "prediction_models_multi":
+        download_and_extract(GDRIVE_IDS['predict_multi'])
+    elif mode == "train_data_single":
+        download_xlnet_data()
+        download_gdrive_docs(GDRIVE_IDS['train_data_single_model'])
+    elif mode == "train_data_multi":
+        download_xlnet_data()
+        download_gdrive_docs(GDRIVE_IDS['train_data_multi_model'])
+
+    logger.info("Finished downloading all the data")
 
 
 if __name__ == "__main__":
--- codebase/settings.py
+++ codebase/settings.py
@@ -1,16 +1,18 @@
 """
 Settings for the application
 """
-import os
+import sys
 from pathlib import Path
 
 PACKAGE_ROOT = Path(__file__).resolve().parent.parent
+sys.path.append(PACKAGE_ROOT)
 
 DATA_PATH = PACKAGE_ROOT.joinpath("data")
 MODEL_PATH = PACKAGE_ROOT.joinpath("models")
 MISC_PATH = PACKAGE_ROOT.joinpath("misc")
 
 XLNET_BASE_PATH = MODEL_PATH.joinpath("xlnet-base-cased")
+OUT_MODELS_PATH = MODEL_PATH.joinpath("out")
 VOCABULARY_PATH = XLNET_BASE_PATH.joinpath("spiece.model")
 
 LOOKUP_PKL_FILENAME = MISC_PATH.joinpath("hierarchy_lookup_dict.pkl")
